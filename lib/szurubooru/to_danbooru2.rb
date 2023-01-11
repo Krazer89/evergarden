d2696ed6e9a513032842986746344e70e2c06125
Krazer89 <jamie@krazer.one>
2023-01-12 00:57:30 +1300

    Remove underscores from pool names

--- lib/szurubooru/to_danbooru2.rb
+++ lib/szurubooru/to_danbooru2.rb
@@ -32,7 +32,7 @@ module Szurubooru
 
       {
         id: body["id"],
-        name: body["names"][0],
+        name: body["names"][0].humanize(capitalize: false, keep_id_suffix: true),
         description: body["description"] ||= "",
         created_at: body["creationTime"],
         updated_at: body["lastEditTime"],
